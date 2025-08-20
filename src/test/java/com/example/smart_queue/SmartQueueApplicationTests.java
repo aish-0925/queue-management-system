f9c9dc9c71cda848ba2e9cbe5056f8a256aff5b9
aish-0925 <aishwaryaapoojary51@gmail.com>
2025-08-20 22:42:31 +0530

    initial commit

---
+++ src/test/java/com/example/smart_queue/SmartQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
