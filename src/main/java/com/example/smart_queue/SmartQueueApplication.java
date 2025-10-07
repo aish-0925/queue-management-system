a43f24b49ada92555b5f74a3d6d7015bbdf7b4b7
aish-0925 <aishwaryaapoojary51@gmail.com>
2025-10-07 14:16:01 +0530

    Formated main method for consistency and readability

--- src/main/java/com/example/smart_queue/SmartQueueApplication.java
+++ src/main/java/com/example/smart_queue/SmartQueueApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartQueueApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SmartQueueApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SmartQueueApplication.class, args);
+    }
 }
